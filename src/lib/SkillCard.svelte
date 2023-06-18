e41fa36dc6ccce3e6f1a25b0114ada084585d9b3
M3DZIK <me@medzik.dev>
2023-06-18 14:18:12 +0200

    Rewrite website

---
+++ src/lib/SkillCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let icon: any;
+    export let title: string;
+    export let description: string;
+</script>
+
+<div class="card variant-glass p-4 md:p-10 shadow-lg space-y-4 text-center">
+    <svg
+        class="mx-auto variant-glass rounded-2xl p-2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 50 50"
+        width="50px" height="50px"
+    >
+        <svelte:component this={icon} />
+    </svg>
+
+    <h3 class="h3">{title}</h3>
+
+    <p>{description}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+	import SkillCard from "$lib/SkillCard.svelte";
+	import Java from "$lib/icons/Java.svelte";
+	import Golang from "$lib/icons/Golang.svelte";
+	import JavaScript from "$lib/icons/JavaScript.svelte";
+	import Svelte from "$lib/icons/Svelte.svelte";
+
+	import avatar from '../assets/avatar.avif'
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="max-w-[640px] variant-glass rounded-container-token p-6 grid grid-cols-3 gap-8 items-center shadow-xl">
+		<img src={avatar} class="w-full aspect-square rounded-full" alt="Avatar" />
+
+		<div class="col-span-2 space-y-2">
+			<div class="text-on-primary-token text-lg opacity-75">Self-Taught Programmer</div>
+
+			<div class="text-on-primary-token font-bold text-xl sm:text-3xl">Oskar</div>
+
+			<div class="text-on-primary-token text-lg sm:text-2xl">Creating the Future</div>
+		</div>
+	</div>
+</div>
+
+<div class="min-h-full mx-auto flex justify-center items-center variant-filled-primary p-16">
+	<div class="container space-y-10">
+		<h2 class="h2 text-center card-header">Languages</h2>
+
+		<div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+			<SkillCard
+				icon={Java}
+				title="Java/Kotlin"
+				description="After several years of writing code, Java and Kotlin seem to work best for me."
+			/>
+
+			<SkillCard
+				icon={Golang}
+				title="Go"
+				description="Golang is my second favorite language because it is fast and friendly to write."
+			/>
+
+			<SkillCard
+				icon={JavaScript}
+				title="JavaScript"
+				description="Unfortunately, the use of JavaScript is a must in web development."
+			/>
+
+			<SkillCard
+				icon={Svelte}
+				title="Svelte"
+				description="However, sometimes when I need to write a website, I use Svelte."
+			/>
+		</div>
+	</div>
+</div>
