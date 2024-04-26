9e162fab4c0c8656c7d2875ec64aad5559928340
M3DZIK <me@medzik.dev>
2024-04-26 18:49:45 +0200

    Update librepass card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -77,11 +77,19 @@
 
         <div class="flex justify-center">
             <a href="https://librepass.org" target="_blank" class="max-w-xl">
-                <Card
-                    icon={MdSecurity}
-                    title="LibrePass"
-                    description="LibrePass is an open-source password manager that prioritizes security and ease of use."
-                />
+                <div class="card variant-filled-error bg-opacity-25 md:p-10 shadow-lg space-y-4 text-center btn whitespace-normal grid">
+                    <div class="mx-auto variant-filled-error bg-opacity-50 rounded-2xl p-2" style="width: 50px; height: 50px;">
+                        <MdSecurity />
+                    </div>
+                
+                    <h3 class="h3">
+                        LibrePass
+                    </h3>
+                
+                    <p class="opacity-85">
+                        LibrePass is an open-source password manager that prioritizes security and ease of use.
+                    </p>
+                </div>
             </a>
         </div>
     </div>
